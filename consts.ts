76244261a7dba1a426165b09c49cb5b14ca668be
Dex REAU <dev@oqva.digital>
2023-12-04 17:15:38 -0300

    up

--- consts.ts
+++ consts.ts
@@ -1,11 +1,11 @@
 
 // META
 
-export const SITE_TITLE = "";
+export const SITE_TITLE = "Broken Dreams · Beyond the Brew";
 
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "Discover our mobile coffee cafes as more than just a stop for your caffeine fix. Find a unique coffee experience near you today.";
 
-export const SITE_KEYWORDS = ""
+export const SITE_KEYWORDS = "Coffee, Mobile Cafe, Authentic, Aspiration, Community, Baristas, Art, Live Performances, Caffeine Fix, Diversity, Original, Taste of Inspiration, Stories, Connection, Unique Experience, London Coffee"
     
 
 // SOCIAL SHARING IMAGE
