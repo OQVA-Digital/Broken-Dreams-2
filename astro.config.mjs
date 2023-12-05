86db68deb7bb47a8e394adf8f79894458d87554e
Dex REAU <dev@oqva.digital>
2023-12-05 07:51:11 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://template-domain',
+  site: 'https://brokendreams.coffee/',
   integrations: [sitemap()],
 });
